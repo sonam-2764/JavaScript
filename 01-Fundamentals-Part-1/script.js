a39842ccce8c1f56152f5b526e7f6b4eefbd778b
Sonam Kumari <aspritualgirl@gmail.com>
2025-06-16 16:34:24 +0530

    js codes

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,4 +1,6 @@
 let js = 'amazing';
-      if(js == 'amazing') alert('Javascript is FUN!');
-      40+8 + 23 -10;
-      console.log(40+8 + 23 -10);
+console.log(40+8 + 23 -10);
+console.log("Jonas");
+
+let firstName = "Matidla";
+console.log(firstName);
