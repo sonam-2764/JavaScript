3a5a097818f45044daeea0ef5d5a8f9b996fece1
Sonam Kumari <aspritualgirl@gmail.com>
2025-06-17 19:07:43 +0530

    assignment1

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -4,3 +4,17 @@ console.log("Jonas");
 
 let firstName = "Matidla";
 console.log(firstName);
+
+let country = "India";
+let continent ="Asia";
+const population = "1.4 billion";
+console.log(country);
+console.log(continent);
+console.log(population);
+
+let isIsland = true;
+var language;
+console.log(typeof isIsland, population, country, language);
+language = "Hindi";
+console.log(language);
+population = 1;
