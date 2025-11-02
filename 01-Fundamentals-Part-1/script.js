8f21151b59a69d112f506c6f80dcb348dc175d23
Sonam Kumari <aspritualgirl@gmail.com>
2025-11-02 09:10:18 +0530

    updated the structure

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -104,11 +104,13 @@ const massMark = 95;
 const heightMark = 1.88;
 const masssJohn = 85;
 const heightJohn = 1.76;
+
 const BMIMark = massMark / heightMark ** 2;
 const BMIJohn = massJohn / (heightJohn * heightJohn);
-if(BMIMark > BMIJohn){
+
+if (BMIMark > BMIJohn) {
     console.log("Mark's BMI is higher than John's!")
-}else{
+} else {
     console.log("John's BMI is higher than Mark's!")
 }
 
