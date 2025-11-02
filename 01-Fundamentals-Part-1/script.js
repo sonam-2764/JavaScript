156aeea612144e51f71a1d1ceb9cc1a99d1772fd
Sonam Kumari <aspritualgirl@gmail.com>
2025-11-02 08:59:44 +0530

    challenge

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,98 +1,113 @@
-let js = 'amazing';
-console.log(40+8 + 23 -10);
-console.log("Jonas");
+// let js = 'amazing';
+// console.log(40+8 + 23 -10);
+// console.log("Jonas");
 
-let firstName = "Matidla";
-console.log(firstName);
+// let firstName = "Matidla";
+// console.log(firstName);
 
-let country = "India";
-let continent ="Asia";
-const population = "1400000000";
-console.log(country);
-console.log(continent);
-console.log(population);
+// let country = "India";
+// let continent ="Asia";
+// const population = "1400000000";
+// console.log(country);
+// console.log(continent);
+// console.log(population);
 
-let isIsland = true;
-var language;
-console.log(typeof isIsland, population, country, language);
-language = "Hindi";
-console.log(language);
+// let isIsland = true;
+// var language;
+// console.log(typeof isIsland, population, country, language);
+// language = "Hindi";
+// console.log(language);
 
 
-const nameFirst = "Shraddha";
-const lastName = "Pratyaksh";
-console.log(nameFirst+' '+ lastName);
-//2 ** 3 means 2 to the power of 3 = 2*2*2
+// const nameFirst = "Shraddha";
+// const lastName = "Pratyaksh";
+// console.log(nameFirst+' '+ lastName);
+// //2 ** 3 means 2 to the power of 3 = 2*2*2
 
-const ageabc = "32";
-const agexyz = "43";
-console.log(ageabc > agexyz);
-const isFullAge = ageabc > agexyz;
-console.log(isFullAge);
+// const ageabc = "32";
+// const agexyz = "43";
+// console.log(ageabc > agexyz);
+// const isFullAge = ageabc > agexyz;
+// console.log(isFullAge);
 
-let population2 = population/2;
-console.log(population2);
+// let population2 = population/2;
+// console.log(population2);
 
-console.log(population2 + 1);
-const populationFinland = "6000000";
-console.log(population2 > populationFinland);
-const avgpopulation = "33000000";
-console.log(population2 < avgpopulation);
-let description = "Portugal is in Europe, and its 11 million people speak Portuguese";
-console.log(description);
+// console.log(population2 + 1);
+// const populationFinland = "6000000";
+// console.log(population2 > populationFinland);
+// const avgpopulation = "33000000";
+// console.log(population2 < avgpopulation);
+// let description = "Portugal is in Europe, and its 11 million people speak Portuguese";
+// console.log(description);
  
 
-var massOfMark = 78;
-var heightOfMark = 1.69;
-var massOfJohn = 92;
-var heightOfJohn = 1.95;
+// var massOfMark = 78;
+// var heightOfMark = 1.69;
+// var massOfJohn = 92;
+// var heightOfJohn = 1.95;
 
-var BMIOfMark = massOfMark / heightOfMark ** 2;
-var BMIOfJohn = massOfJohn / (heightOfJohn * heightOfJohn);
+// var BMIOfMark = massOfMark / heightOfMark ** 2;
+// var BMIOfJohn = massOfJohn / (heightOfJohn * heightOfJohn);
 
-let markHigherBMI = BMIOfMark > BMIOfJohn;
-console.log(BMIOfMark, BMIOfJohn, markHigherBMI);
+// let markHigherBMI = BMIOfMark > BMIOfJohn;
+// console.log(BMIOfMark, BMIOfJohn, markHigherBMI);
 
-massOfMark = 95;
-heightOfMark = 1.88;
-massOfJohn = 85;
-heightOfJohn = 1.76;
- BMIOfMark = massOfMark / heightOfMark ** 2;
- BMIOfJohn = massOfJohn / (heightOfJohn * heightOfJohn);
+// massOfMark = 95;
+// heightOfMark = 1.88;
+// massOfJohn = 85;
+// heightOfJohn = 1.76;
+//  BMIOfMark = massOfMark / heightOfMark ** 2;
+//  BMIOfJohn = massOfJohn / (heightOfJohn * heightOfJohn);
 
- console.log(BMIOfMark, BMIOfJohn, markHigherBMI);
+//  console.log(BMIOfMark, BMIOfJohn, markHigherBMI);
 
- const myName = "Maitri";
- const job = "student";
- const birthyear = 2005;
- const year = 2025;
- const Maitri = "I'm " + myName + ',a '+ (year-birthyear) +' years old '+ job +'!';
- console.log(Maitri);
-// for using template literals we need to use the symbol above the tab button 
-const MaitriNew = `I'm ${myName}, a ${year-birthyear} years old ${job}!`;
-console.log(MaitriNew);
-console.log(`Just a regular string...`);
-console.log('String with \n\
-multiple \n\
-lines');
-console.log(`String 
-multiple
-lines 
-using 
-this button`);
+//  const myName = "Maitri";
+//  const job = "student";
+//  const birthyear = 2005;
+//  const year = 2025;
+//  const Maitri = "I'm " + myName + ',a '+ (year-birthyear) +' years old '+ job +'!';
+//  console.log(Maitri);
+// // for using template literals we need to use the symbol above the tab button 
+// const MaitriNew = `I'm ${myName}, a ${year-birthyear} years old ${job}!`;
+// console.log(MaitriNew);
+// console.log(`Just a regular string...`);
+// console.log('String with \n\
+// multiple \n\
+// lines');
+// console.log(`String 
+// multiple
+// lines 
+// using 
+// this button`);
 
-/*const age = 19;
-const isOldEnough = age >= 18;
-if(isOldEnough){
-    console.log("Maitri can drive🚗");// windows + . = emoji
-} 
-    or*/
+// /*const age = 19;
+// const isOldEnough = age >= 18;
+// if(isOldEnough){
+//     console.log("Maitri can drive🚗");// windows + . = emoji
+// } 
+//     or*/
 
-const age = 15;
+// const age = 15;
 
-if(age>=18){
-    console.log("Maitri can drive🚗");// windows + . = emoji
-} else{
-    const yearsLeft = 18- age;
-    console.log(`maitri can drive after ${yearsLeft} years `);
+// if(age>=18){
+//     console.log("Maitri can drive🚗");// windows + . = emoji
+// } else{
+//     const yearsLeft = 18- age;
+//     console.log(`maitri can drive after ${yearsLeft} years `);
+// }
+
+
+
+//challenge 2
+const massMark = 95;
+const heightMark = 1.88;
+const masssJohn = 85;
+const heightJohn = 1.76;
+const BMIMark = massMark / heightMark ** 2;
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+if(BMIMark > BMIJohn){
+    console.log("Mark's BMI is higher than John's!")
+}else{
+    console.log("John's BMI is higher than Mark's!")
 }
