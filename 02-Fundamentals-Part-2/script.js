a92d44a46f1090ed1badd06adfac2ec2ff0d5024
Sonam Kumari <aspritualgirl@gmail.com>
2026-03-21 12:08:52 +0530

    functions

---
+++ 02-Fundamentals-Part-2/script.js
@@ -0,0 +1,39 @@
+'use strict';
+//  let hasLicense = false;
+//  const passTest = true;
+//  if(passTest) 
+//     haLicense = true;
+//  if(hasLicense)
+//     console.log('I can drive');//see by deactivating and activating use strict 
+
+
+
+
+
+
+// //function L3
+// function logger(){
+//     console.log("My name is sonam ");
+// }
+// //calling, running or invoking function
+// logger();
+// logger();
+// function fruitProcessor(apples, oranges){
+//     console.log(apples,oranges);
+//     const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+//     return juice;
+// }
+// const appleJuice = fruitProcessor(5,0);
+// console.log(appleJuice);// for returning juice we need to store the value in some variable
+// console.log(fruitProcessor(5,0));//this will also give same result
+
+// const appleOrangeJuice = fruitProcessor(2,4);
+// console.log(appleOrangeJuice);
+
+// // function assignment
+// function describeCountry(country, population, capitalCity){
+//     return `${country} has ${population} people and its capital city is ${capitalCity}`;
+// }
+// const data1 = describeCountry('India', 30000000, 'NewDelhi');
+// const data2 = describeCountry('Canada', 1000000, 'newYork');
+// console.log(data1, data2);
